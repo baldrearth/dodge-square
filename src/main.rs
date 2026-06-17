fe4c0a033197b49b737f326a0cfdde62ab627dbb
baldrearth <asubaldr@gmail.com>
2026-06-17 14:08:54 +0900

    Add player sprite

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Player;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Player,
+        Sprite::from_color(Color::srgb(0.3, 0.7, 1.0), Vec2::new(60.0, 60.0)),
+        Transform::from_xyz(0.0, -250.0, 0.0),
+    ));
 }
